7d76a609cb23482de41f27d3d46be55fef973c0a
YogaArifShuhada <yoga57720@gmail.com>
2024-05-25 10:19:39 -0700

    belajarlaravel

---
+++ belajarlaravel/resources/views/page/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>document</title>
+    </head>
+    <body>
+        <h1>SELAMAT DATANG!</h1>
+        <h3>Terima kasih telah bergabung di sanberbook. Social Media kita bersama!</h3>
+    
+
+       <a href="/">kembali</a>
+    </body>
+</html>
